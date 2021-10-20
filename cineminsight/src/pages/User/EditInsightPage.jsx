879dce78de8ec10e73ddc3046eab6133978f6e7c
seantolbert <84528717+seantolbert@users.noreply.github.com>
2021-10-20 15:44:19 -0400

    adds user functionality

--- cineminsight/src/pages/User/EditInsightPage.jsx
+++ cineminsight/src/pages/User/EditInsightPage.jsx
@@ -1,8 +1,79 @@
-export default function EditInsightPage() {
-    return (
-      <main>
-        <div>EditInsightPage</div>
-      </main>
-    );
-  }
-  
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const ratingList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+export default function EditInsightPage({ handleUpdateInsight, cinemas }) {
+  const location = useLocation();
+  const [invalidForm, setValidForm] = useState(true);
+  const [editedInsight, setEditedInsight] = useState(
+    location.state.insightItem
+  );
+  const formRef = useRef();
+
+  useEffect(() => {
+    formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
+  }, [editedInsight]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleUpdateInsight(editedInsight);
+  };
+
+  const handleChange = (e) => {
+    setEditedInsight({
+      ...editedInsight,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <main>
+      <h1>Edit Insight</h1>
+      <form autoComplete="off" ref={formRef} onSubmit={handleSubmit}>
+        <div>
+          <label>Cinema</label>
+          <select
+            name="game"
+            value={editedInsight.game}
+            onChange={handleChange}
+            required
+          >
+            {cinemas.map((cinema) => {
+              return <option value={cinema.title}>{cinema.title}</option>;
+            })}
+          </select>
+        </div>
+        <div>
+          <label>Rating</label>
+          <select
+            name="rating"
+            value={editedInsight.rating}
+            onChange={handleChange}
+            required
+          >
+            {ratingList.map((rating) => {
+              return <option value={rating}>{rating}</option>;
+            })}
+          </select>
+        </div>
+        <div>
+          <label>Review:</label>
+          <textarea
+            name="review"
+            value={editedInsight.review}
+            onChange={handleChange}
+          />
+        </div>
+        <input type="hidden" name="user" value={editedInsight.user} />
+        <button type="submit" disabled={invalidForm}>
+          EDIT Insight
+        </button>
+        &nbsp;&nbsp;
+        <Link to="/user">
+          <button>CANCEL</button>
+        </Link>
+      </form>
+    </main>
+  );
+}
--- cineminsight/src/pages/User/UserProfilePage.jsx
+++ cineminsight/src/pages/User/UserProfilePage.jsx
@@ -1,8 +1,26 @@
-export default function UserProfilePage() {
-    return (
-      <main>
-        <div>UserProfilePage</div>
-      </main>
-    );
-  }
-  
+import UserCard from "../../components/User/UserCard";
+import UserInsights from "../../components/User/UserInsights";
+import { Link } from "react-router-dom";
+
+export default function UserProfilePage({
+  handleDeleteInsight,
+  insights,
+  user,
+}) {
+  return (
+    <main>
+      <h1>Profile</h1>
+      <UserCard user={user} />
+      <Link to="user/insights/newinsight">
+        <button>New Insight</button>
+      </Link>
+      <h2>Insights</h2>
+      <hr />
+      <UserInsights
+        handleDeleteInsight={handleDeleteInsight}
+        insights={insights}
+        user={user}
+      />
+    </main>
+  );
+}
