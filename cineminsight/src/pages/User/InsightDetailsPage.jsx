c0100c076de5a844120fecf87d7572acac06151b
seantolbert <84528717+seantolbert@users.noreply.github.com>
2021-10-21 12:55:10 -0400

    adds full crud for insighta

--- cineminsight/src/pages/User/InsightDetailsPage.jsx
+++ cineminsight/src/pages/User/InsightDetailsPage.jsx
@@ -1,8 +1,16 @@
+import React from "react";
+import UserInsightDetail from "../../components/User/UserInsightDetail";
+import { useLocation } from "react-router-dom";
+
 export default function InsightDetailsPage() {
-    return (
-      <main>
-        <div>InsightDetailPage</div>
-      </main>
-    );
-  }
-  
+  const {
+    state: { insightItem },
+  } = useLocation();
+
+  return (
+    <main>
+      <h1>Insight Details</h1>
+      <UserInsightDetail insight={insightItem} key={insightItem._id} />
+    </main>
+  );
+}
